accf203afa287e226081be2bc7fec9b2c3434316
Pedro Girardi <pedro@plando.work>
2021-01-08 21:06:36 +0700

    Clean up

--- src/dev/clojure/user.clj
+++ src/dev/clojure/user.clj
@@ -11,35 +11,8 @@
 
   (reset)
 
-  ;; -- Client API.
-
-  (http/post "http://localhost:8080/api/v1/transact"
-             {:body (server/transit-encode [(literate/code "Hello")])})
-
-  (http/post "http://localhost:8080/api/v1/transact"
-             {:body (server/transit-encode [(literate/vega-lite
-                                             {"$schema" "https://vega.github.io/schema/vega-lite/v4.json"
-                                              :description "A simple bar chart with embedded data."
-                                              :data {:values
-                                                     [{:a "A" :b 28}
-                                                      {:a "B" :b 55}
-                                                      {:a "C" :b 43}
-                                                      {:a "D" :b 91}
-                                                      {:a "E" :b 81}
-                                                      {:a "F" :b 53}
-                                                      {:a "G" :b 19}
-                                                      {:a "H" :b 87}
-                                                      {:a "I" :b 52}]}
-                                              :mark "bar"
-                                              :encoding {:x {:field "a"
-                                                             :type "ordinal"}
-                                                         :y {:field "b"
-                                                             :type "quantitative"}}})])})
-
-
 
   ;; -- Vega Lite.
-
 
   (literate/view
    (literate/vega-lite
